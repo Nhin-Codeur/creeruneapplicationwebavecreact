6077f22f283febd98b8bc4b67cf62fec24649934
Elliot <clemenceauelliot@gmail.com>
2023-11-14 16:16:26 +0100

    hash router

--- src/components/accueil.jsx
+++ src/components/accueil.jsx
@@ -11,32 +11,32 @@ function Accueil() {
   const [locationsList, setLocationsList] = useState([]);
 
   useEffect(() => {
-      var json = require('../assets/locations-request.json')
-      setLocationsList(json);
-      console.log(json)
+    var json = require('../assets/locations-request.json')
+    setLocationsList(json);
+
   }, [])
 
   return (
-      <div>
-        <header className="App-header">
-          <div className='home-pub'>
-            <img src={HomeTopPicture} className="home-top-picture" alt="home-top-picture" />
-            <span> Chez vous, partout, et ailleurs </span>
-          </div>
-          <div className='home-gallery'>
-            {
-              locationsList.map((location) => {
-                return <LocationCard titre={location.title} cover={location.cover} id={location.id}></LocationCard>
-                
-              })
-            }
-          </div>
-          
-        </header>
-        
-        
-      </div>
-      
+    <div>
+      <header className="App-header">
+        <div className='home-pub'>
+          <img src={HomeTopPicture} className="home-top-picture" alt="home-top-picture" />
+          <span> Chez vous, partout, et ailleurs </span>
+        </div>
+        <div className='home-gallery'>
+          {
+            locationsList.map((location) => {
+              return <LocationCard titre={location.title} cover={location.cover} id={location.id}></LocationCard>
+
+            })
+          }
+        </div>
+
+      </header>
+
+
+    </div>
+
   );
 }
 
